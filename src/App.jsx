b08cadd1f72ff0abdd59807f5a70d4f5a7fd25a1
HaydenNease <hayden.nease@gmail.com>
2023-02-17 14:59:58 -0500

    Updates

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "bootswatch/dist/lux/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-
-import PortfolioContainer from "./components/PortfolioContainer";
-
-const App = () => <PortfolioContainer/>;
-
-export default App
