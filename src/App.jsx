b079f67da3ceb4c60b66fa945d64b8dc51da1fb2
HaydenNease <hayden.nease@gmail.com>
2023-03-26 12:27:03 -0400

    bye bye bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import "bootswatch/dist/lux/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 import PortfolioContainer from "./components/PortfolioContainer";
--- src/components/PortfolioContainer/index.jsx
+++ src/components/PortfolioContainer/index.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import NavTabs from '../NavTabs';
 import Home from '../pages/Home';
 import Projects from '../pages/Projects';
@@ -29,14 +26,10 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <Container className="bg-white border-dark border-0 rounded">
+    <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      <Row>
-        <Col className='container-fluid'>
-          {/* Here we are calling the renderPage method which will return a component  */}
-          {renderPage()}
-        </Col>
-      </Row>
-    </Container>
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
   );
 }
