87d48b88eb74d0e67c81ddfecff1abad6504edf6
HaydenNease <hayden.nease@gmail.com>
2023-03-28 12:05:40 -0400

    scroll position is tracking

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,35 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 import { NavTabs, Footer, Home, Projects, Resume, Contact  } from './components';
 
+
 function App() {
   const [currentPage, setCurrentPage] = useState('Home');
+
+  const useScrollPosition = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+  
+    useEffect(() => {
+      const updatePosition = () => {
+        setScrollPosition(window.pageYOffset);
+      }
+      window.addEventListener("scroll", updatePosition);
+      updatePosition();
+      return () => window.removeEventListener("scroll", updatePosition);
+    }, []);
+    
+    return scrollPosition;
+  };
+  console.log(useScrollPosition());
+
+  const hideBody = () => {
+    if (useScrollPosition() === 0) {
+      return "hidden";
+    } else {
+      return "visible";
+    }
+  }
+  hideBody();
+  
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
