cea3286808cb8cc96ae71489e1bf8502cc429e64
HaydenNease <hayden.nease@gmail.com>
2023-04-01 18:33:34 -0400

    oh boy here goes nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'
-import { NavTabs, Footer, Home, Projects, Resume, Contact  } from './components';
-
-
-function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const useScrollPosition = () => {
-    const [scrollPosition, setScrollPosition] = useState(0);
-  
-    useEffect(() => {
-      const updatePosition = () => {
-        setScrollPosition(window.pageYOffset);
-      }
-      window.addEventListener("scroll", updatePosition);
-      updatePosition();
-      return () => window.removeEventListener("scroll", updatePosition);
-    }, []);
-    
-    return scrollPosition;
-  };
-  console.log(useScrollPosition());
-
-  
-  
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home/>;
-    }
-    if (currentPage === 'Projects') {
-      return <Projects/>;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume/>;
-    }
-    return <Contact/>;
-  };
-  const hideBody = () => {
-    if (useScrollPosition() === 0) {
-      return <div/>
-    } else {
-      return renderPage();
-    }
-  }
-  hideBody();
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
+const App = () => {
   return (
     <div>
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
-      {hideBody()}
-      <Footer/>
-      <div>poop</div>
+      3D Developer Portfolio
     </div>
   );
 };
--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Contact from "./Contact";
-import Home from "./Home";
-import Projects from "./Projects";
-import Resume from "./Resume";
-
-export {Contact, Home, Projects, Resume};
